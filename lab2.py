7572fd95976a4d63e7e582e39ff41469f037a5ac
Eunbyul <89649741+EunByu1@users.noreply.github.com>
2022-08-04 19:30:25 +0900

    Delete lab2.py

--- lab2.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-students = []
-
-for _ in range(int(input())):
-    name, day, month, year = input().split()
-    students.append([name, int(day), int(month), int(year)])
-
-print(students)
